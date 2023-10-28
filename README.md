758a278cd73bad2ed08e285588cd2ca6f51244c7
Tajuar <tajuarb@umich.edu>
2023-10-28 16:11:19 -0400

    current progress on autotune

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# eecs452
-452 project
+# eecs452  Project
+
+## Prototyping Branch (Python)
+
+- audio_test.py
+> For testing the overall system scheme from Stage 1 to Stage 2
+
+## Stage 1 (Auto-Tune/Vocoder Selector)
+
+- auto_tune.py 
+> Auto-tune prototype
+
+- vocoder.py
+> Vocoder Prototype
+
+## Stage 2 (Pass data into distortion and reverb)
+
+- reverb.py 
+> Adjust things like echo (adding smaller versions of original signal waves with adjusted ITD/ILD)
+
+- distortion.py
+> Aggressive Square clipping
+
+## Stage 3 (output)
+
+- audio_test.py
--- auto_tune.py
+++ auto_tune.py
@@ -1,3 +1,7 @@
+import numpy as np
+import aubio # https://github.com/aubio/aubio
+import pyaudio
+
 # Auto-Tune Prototype
 
 # STFT (Pitch Detection)
@@ -7,3 +11,67 @@
 # Formant Preservation
 
 # Note Transition
+
+semitone_distance = 2**(1/12)
+sample_rate = 44100 # Khz
+chunk_size = 882
+
+def autotune(mic_signal, formant_flag=0, method=0): # input signal 44.1Khz, bool formant-preservation, method
+    if formant_flag == 1:
+        return autotune_original(mic_signal)
+    if method == 0:
+        return autotune_cepstrum(mic_signal)
+    if method == 1:
+        return autotune_psola(mic_signal)
+    if method == 2:
+        return autotune_lpc(mic_signal)
+    return
+
+# find the nearest frequency
+def shift_to_nearest_semitone(note_frequency):
+        # Calculate the nearest semitone frequency
+        nearest_semitone = round(12 * np.log2(note_frequency / 440))
+        nearest_frequency = 440 * 2**(nearest_semitone / 12)
+        return nearest_frequency
+
+# basic pitch shifting no formant preservation
+def autotune_original(mic_signal):
+    # find the main frequency of the chunk
+
+    # find the nearest semitone
+
+    # shift it to that semitone
+
+    return 
+
+#Cepstrum (https://en.wikipedia.org/wiki/Cepstrum)
+def autotune_cepstrum(mic_signal): 
+
+    # do pitch shifting
+
+    # Create envelope from old vs. new version of signal and apply correction envelope
+
+    return
+
+
+# PSOLA https://en.wikipedia.org/wiki/PSOLA
+def autotune_psola(mic_signal):
+
+    # Detect the note as they come in
+
+    # Shift to intended target note
+
+    # Create envelope from old vs. new version of signal and apply correction envelope
+
+    return
+
+# Linear Predictive Coding (https://en.wikipedia.org/wiki/Linear_predictive_coding)
+def autotune_lpc(mic_signal): 
+
+    # Detect the note as they come in
+
+    # Shift to intended target note
+
+    # Create envelope from old vs. new version of signal and apply correction envelope
+
+    return
