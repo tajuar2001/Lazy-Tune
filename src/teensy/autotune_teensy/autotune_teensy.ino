f260c15e25d15acc0bbaf298a6031a45dfe945fe
tajuarb <tajuarb@umich.edu>
2023-11-11 17:01:05 -0500

    updated the .ino file and made some comments

--- src/teensy/autotune_teensy/autotune_teensy.ino
+++ src/teensy/autotune_teensy/autotune_teensy.ino
@@ -3,24 +3,67 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
-
 #include "autotune.h"
 
-// Create an instance of the Autotune class
+// Audio Components and Autotune Instance
+AudioInputI2S i2s1; // Assuming you're using I2S for audio input
+// ... (Other audio components if necessary)
 Autotune autotuner;
 
+// Buffer to hold audio data for autotune processing
+const size_t signalLength = 1024;
+int16_t micSignal[signalLength];
+
+// Function declarations
+void processAudio(); // Function to process and autotune audio
+void readAndExecuteCommands(); // Function to read serial commands and execute them
+
 void setup() {
-    Serial.begin(9600);
-    // Additional setup code goes here
+    Serial.begin(115200); // Start serial communication at a higher baud rate
+    AudioMemory(12); // Allocate memory for audio processing
+    // Additional setup code for audio components goes here
 }
 
 void loop() {
-    // Your main loop code goes here
+    processAudio(); // Process and autotune the audio
+    readAndExecuteCommands(); // Check for and execute serial commands
+}
 
-    // autotuneOriginal 
-    const size_t signalLength = 1024;  
-    int16_t micSignal[signalLength];   // Replace this with your actual input signal
+// Function to process and autotune audio
+void processAudio() {
+    // Here you would add code to fill micSignal with actual audio data
+    // For now, we're assuming micSignal is filled elsewhere in your code
+
+    // Once micSignal is filled with audio data, you can process it
     autotuner.autotuneOriginal(micSignal, signalLength);
 
-    // Additional loop code goes here
+    // Additional processing and output code goes here
 }
+
+// Function to read serial commands and execute them
+void readAndExecuteCommands() {
+    if (Serial.available()) {
+        String command = Serial.readStringUntil('\n');
+        executeCommand(command);
+    }
+}
+
+// Execute a received command
+void executeCommand(const String& command) {
+    // Here, you'll parse the command and apply settings to the autotuner
+    if (command.startsWith("autotune:")) {
+        // Parse the command to get the autotune parameters
+        // Example command might be "autotune:on" or "autotune:off"
+        String parameter = command.substring(9);
+        if (parameter == "on") {
+            // Turn on autotune
+        } else if (parameter == "off") {
+            // Turn off autotune
+        }
+        // Add other parameters as needed
+    } else {
+        Serial.println("Unknown command");
+    }
+}
+
+// Add other functions as needed, for example to set autotune parameters
