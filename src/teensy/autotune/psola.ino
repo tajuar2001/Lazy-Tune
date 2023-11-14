e5e8b75815e983675cae6cbb157c1b1e90ae3671
tajuarb <tajuarb@umich.edu>
2023-11-14 16:57:57 -0500

    more updates

--- src/teensy/autotune/psola.ino
+++ src/teensy/autotune/psola.ino
@@ -14,35 +14,23 @@ public:
     // 1. Time-domain pitch detection:
     //    Analyze the incoming audio signal to determine the pitch periods.
     //    This could be done using auto-correlation or a zero-crossing method.
-    //    For this example, we will use a zero-crossing method.
-    //    The zero-crossing method is simple and works well for monophonic signals.
-
+  
     // 2. Divide the audio into pitch periods:
     //    Split the audio into separate grains, each corresponding to a single pitch period.
     //    This step is crucial for the subsequent time-domain processing.
-    //    For this example, we will use a fixed grain size of 256 samples.
-    //    This means that the pitch period must be a multiple of 256 samples.
-    //    The pitch period is determined by the zero-crossing method in step 1.
 
     // 3. Time stretching or compression:
     //    Depending on the desired effect, each grain can be stretched (for a lower pitch)
     //    or compressed (for a higher pitch) in time without altering its spectral characteristics.
-    //    For this example, we will use a fixed grain size of 256 samples.
-    //    This means that the pitch period must be a multiple of 256 samples.
-    //    The pitch period is determined by the zero-crossing method in step 1.
 
     // 4. Overlap and add:
     //    The processed grains are then recombined by overlapping them at the correct intervals.
     //    This step ensures a smooth transition between grains, minimizing artifacts.
-    //    For this example, we will use a fixed grain size of 256 samples.
-    //    This means that the pitch period must be a multiple of 256 samples.
-    //    The pitch period is determined by the zero-crossing method in step 1.
 
 
     // 5. Output the processed block:
     //    The modified audio block is then transmitted for playback or further processing.
 
-    
     transmit(block, 0);
     release(block);
   }
