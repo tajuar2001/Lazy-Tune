ef592d5692858fa249f2d6be12f73a7ebd137e27
tajuarb <tajuarb@umich.edu>
2023-11-15 21:40:01 -0500

    updated display

--- src/raspberry/display/2window.py
+++ src/raspberry/display/2window.py
@@ -1,39 +1,46 @@
 import tkinter as tk
-import pyserial
+
 def main():
     window = tk.Tk()
     window.title("Dynamic Buttons")
+    window.geometry("600x400")  # Set window size
 
     # Function to print a custom message
     def print_message(message):
         print(message)
 
+    # Configure grid for spacing and resizing
+    def configure_grid():
+        for i in range(3):  # Adjust to a 3x3 grid
+            window.rowconfigure(i, weight=1)
+            window.columnconfigure(i, weight=1)
+
+    # Function to show option buttons
     def show_options(button_number):
-        # Clear window
         for widget in window.winfo_children():
             widget.destroy()
+        configure_grid()
 
-        # Manually assign custom messages to each option button
-        tk.Button(window, text="Option 1", command=lambda: print_message(f"Button {button_number} - Option 1")).grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-        tk.Button(window, text="Option 2", command=lambda: print_message(f"Button {button_number} - Option 2")).grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
-        tk.Button(window, text="Option 3", command=lambda: print_message(f"Button {button_number} - Option 3")).grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
-        tk.Button(window, text="Option 4", command=lambda: print_message(f"Button {button_number} - Option 4")).grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
+        # Place option buttons
+        tk.Button(window, text="Option 1", command=lambda: print_message(f"Button {button_number} - Option 1")).grid(row=1, column=0, sticky="nsew")
+        tk.Button(window, text="Option 2", command=lambda: print_message(f"Button {button_number} - Option 2")).grid(row=1, column=1, sticky="nsew")
+        tk.Button(window, text="Option 3", command=lambda: print_message(f"Button {button_number} - Option 3")).grid(row=1, column=2, sticky="nsew")
+        tk.Button(window, text="Option 4", command=lambda: print_message(f"Button {button_number} - Option 4")).grid(row=2, column=1, sticky="nsew")
 
         # Back button
-        tk.Button(window, text="Back", command=show_main_buttons).grid(row=2, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+        tk.Button(window, text="Back", command=show_main_buttons).grid(row=2, column=2, sticky="nsew")
 
+    # Function to show main buttons
     def show_main_buttons():
-        # Clear window
         for widget in window.winfo_children():
             widget.destroy()
+        configure_grid()
 
-        # Manually assign custom messages to each main button
-        tk.Button(window, text="Button 1", command=lambda: [show_options(1), print_message("Main Button 1 clicked")]).grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
-        tk.Button(window, text="Button 2", command=lambda: [show_options(2), print_message("Main Button 2 clicked")]).grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
-        tk.Button(window, text="Button 3", command=lambda: [show_options(3), print_message("Main Button 3 clicked")]).grid(row=0, column=2, sticky="nsew", padx=20, pady=20)
-        tk.Button(window, text="Button 4", command=lambda: [show_options(4), print_message("Main Button 4 clicked")]).grid(row=1, column=0, sticky="nsew", padx=20, pady=20)
-        tk.Button(window, text="Button 5", command=lambda: [show_options(5), print_message("Main Button 5 clicked")]).grid(row=1, column=1, sticky="nsew", padx=20, pady=20)
-        tk.Button(window, text="Button 6", command=lambda: [show_options(6), print_message("Main Button 6 clicked")]).grid(row=1, column=2, sticky="nsew", padx=20, pady=20)
+        # Place main buttons
+        tk.Button(window, text="Button 1", command=lambda: [show_options(1), print_message("Main Button 1 clicked")]).grid(row=1, column=0, sticky="nsew")
+        tk.Button(window, text="Button 2", command=lambda: [show_options(2), print_message("Main Button 2 clicked")]).grid(row=1, column=1, sticky="nsew")
+        tk.Button(window, text="Button 3", command=lambda: [show_options(3), print_message("Main Button 3 clicked")]).grid(row=1, column=2, sticky="nsew")
+        tk.Button(window, text="Button 4", command=lambda: [show_options(4), print_message("Main Button 4 clicked")]).grid(row=2, column=1, sticky="nsew")
 
     show_main_buttons()
     window.mainloop()
---
+++ src/raspberry/display/prodInterface.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+from tkinter import Scale
+
+def main():
+    window = tk.Tk()
+    window.title("Sound Effects Controller")
+    window.geometry("600x400")  # Set window size
+
+    # Function to handle slider value changes
+    def on_slider_change(value, label):
+        print(f"{label} value: {value}")
+
+    # Function to switch to the edit effect screen
+    def show_edit_effect_screen():
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        # Create vertical sliders for effects
+        Scale(window, label="Delay", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Delay")).pack(side="left")
+        Scale(window, label="Flange", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Flange")).pack(side="left")
+        Scale(window, label="Freeverb", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Freeverb")).pack(side="left")
+        Scale(window, label="Chorus", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Chorus")).pack(side="left")
+
+        # Button to switch to distortion tab
+        tk.Button(window, text="Distortion", command=show_distortion_screen).pack(side="left")
+
+        # Back button
+        tk.Button(window, text="Back", command=show_main_screen).pack(side="left")
+
+    # Function to switch to the distortion screen
+    def show_distortion_screen():
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        # Create vertical sliders for distortion effects
+        Scale(window, label="Bitcrush", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Bitcrush")).pack(side="left")
+        Scale(window, label="Overdrive", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Overdrive")).pack(side="left")
+        Scale(window, label="Fuzz", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Fuzz")).pack(side="left")
+        Scale(window, label="Multiplier", from_=0, to=100, orient="vertical", command=lambda value: on_slider_change(value, "Multiplier")).pack(side="left")
+
+        # Back button
+        tk.Button(window, text="Back", command=show_edit_effect_screen).pack(side="left")
+
+    # Function to show the main screen
+    def show_main_screen():
+        for widget in window.winfo_children():
+            widget.destroy()
+
+        # Configure grid for centering with appropriate weights
+        window.rowconfigure(0, weight=0)  # Top row where "Lazy-Tune" and "Edit Effect" will be
+        window.rowconfigure(1, weight=1)
+        window.rowconfigure(2, weight=1)
+        window.columnconfigure(0, weight=1)
+        window.columnconfigure(1, weight=1)
+        window.columnconfigure(2, weight=1)
+        window.columnconfigure(3, weight=1)  # Column for "Edit Effect" button, minimal weight
+
+        # Label for "Lazy-Tune"
+        lazy_tune_label = tk.Label(window, text="Lazy-Tune", font=("Helvetica", 20))
+        lazy_tune_label.grid(row=0, column=0, columnspan=4, sticky="ew")  # Span 3 columns for centering
+
+        # Determine button size
+        button_width = 16
+        button_height = 6
+
+        # Place main buttons in grid, centered below "Lazy-Tune" label
+        tk.Button(window, text="Microphone", width=button_width, height=button_height, command=lambda: print("Microphone button clicked")).grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
+        tk.Button(window, text="Carrier", width=button_width, height=button_height, command=lambda: print("Carrier button clicked")).grid(row=1, column=2, sticky="nsew", padx=10, pady=10)
+        tk.Button(window, text="Autotune", width=button_width, height=button_height, command=lambda: print("Autotune button clicked")).grid(row=2, column=1, sticky="nsew", padx=10, pady=10)
+        tk.Button(window, text="Vocoder", width=button_width, height=button_height, command=lambda: print("Vocoder button clicked")).grid(row=2, column=2, sticky="nsew", padx=10, pady=10)
+
+        # Edit effect button in top right corner
+        edit_effect_button = tk.Button(window, text="Edit Effect", command=lambda: print("Edit Effect button clicked"))
+        edit_effect_button.grid(row=0, column=2, sticky="ne", padx=10, pady=10)
+
+    show_main_screen()
+    window.mainloop()
+
+main()
